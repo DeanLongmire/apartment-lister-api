5928cee26ed6b6eef3681b1e64171089e91bac73
Dean Longmire <deanlongmire100@gmail.com>
2023-06-01 21:49:06 -0400

    Add user to db on register

--- users/controllers/route-logic.js
+++ users/controllers/route-logic.js
@@ -1,9 +1,33 @@
 const { client, dbname } = require("../../database.js");
 
+const usersCollection = client.db(dbname).collection("Users");
+
 const feedback = function (email, password, callback) {
     console.log("Email: " + email);
     console.log("Password: " + password);
     callback();
+}
+
+const insertUser = async (newUser) => {
+    const returned = await usersCollection.insertOne(newUser);
+    return returned;
+}
+
+const buildUser = async (credentials) => {
+    const newUser = {
+        email: credentials.email,
+        password: credentials.password
+    };
+
+    try {
+        const insertedUser = await insertUser(newUser);
+        console.log('User created:', insertedUser.insertedId);
+        return newUser;
+    } 
+    catch (error) {
+        console.error('Error creating user:', error);
+        throw error;
+    }
 }
 
 const login = (req, res) => {     
@@ -14,8 +38,11 @@ const login = (req, res) => {
     });
 }
 
-const register = (req, res) => {
-    res.send("Connected");
+const register = async (req, res) => {
+    const credentials = req.body;
+
+    await buildUser(credentials);
+    res.send("User Created");
 }
 
 module.exports = { login, register };
