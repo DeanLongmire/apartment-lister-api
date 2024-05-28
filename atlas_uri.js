40814761c912149ff9b259e1e95191d2269b2e27
Dean Longmire <deanlongmire100@gmail.com>
2024-05-27 21:41:04 -0400

    Change uri to turn ssl on

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,2 +1,2 @@
 module.exports = uri = 
-    "mongodb+srv://deanlongmire100:gr7V8UsH0BudSHkG@apartments.s63gyey.mongodb.net/?retryWrites=true&w=majority&appName=apartments";
+    "mongodb+srv://deanlongmire100:gr7V8UsH0BudSHkG@apartments.s63gyey.mongodb.net/?retryWrites=true&w=majority&appName=apartments&tls=true";
